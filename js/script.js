6e48b87511bb727eacff75c4805c3a87b3ddffee
manuelmenama <manuel.mena090589@gmail.com>
2022-10-10 16:20:37 +0200

    added comment

--- js/script.js
+++ js/script.js
@@ -1,9 +1,12 @@
+//dichiaro le mie costanti
 const userName = prompt('Inserisci il tuo nome:');
 const userSurname = prompt('Inserisci il tuo cognome:');
 const userColor = prompt('Inserisci il tuo colore preferito:');
 
+//utilities for debug
 console.log(userName, userSurname, userColor);
 
+//stampo in html le stringhe acquisite
 document.getElementById('result').innerHTML += `
 ${userName}${userSurname}${userColor}22
 `;
