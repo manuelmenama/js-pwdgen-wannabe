6508dbd58d4b8b0d7f04b92ee44ea369271b2a84
manuelmenama <manuel.mena090589@gmail.com>
2022-10-10 15:35:48 +0200

    complete

---
+++ js/script.js
@@ -0,0 +1,9 @@
+const userName = prompt('Inserisci il tuo nome:');
+const userSurname = prompt('Inserisci il tuo cognome:');
+const userColor = prompt('Inserisci il tuo colore preferito:');
+
+console.log(userName, userSurname, userColor);
+
+document.getElementById('result').innerHTML += `
+${userName}${userSurname}${userColor}22
+`;
